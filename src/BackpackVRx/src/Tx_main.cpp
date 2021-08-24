ff209b6b03e012cbc18818335bd0723213967cc2
Jye <JyeSmith@users.noreply.github.com>
2021-08-24 17:10:47 +1000

    update tx backpack

--- src/BackpackVRx/src/Tx_main.cpp
+++ src/BackpackVRx/src/Tx_main.cpp
@@ -3,29 +3,30 @@
 #include <espnow.h>
 #include <EEPROM.h>
 
+#define WIFI_PIN 0
+#define LED_PIN 16
+uint8_t flashLED = false;
+
 bool startWebUpdater = false;
 uint8_t channelHistory[3] = {255};
 
-uint8_t broadcastAddress[] = {VRX_MAC};
-
 void OnDataRecv(uint8_t * mac_addr, uint8_t *data, uint8_t data_len)
 {
-  if (broadcastAddress[0] == mac_addr[0] && broadcastAddress[1] == mac_addr[1] && broadcastAddress[2] == mac_addr[2] && broadcastAddress[3] == mac_addr[3] && broadcastAddress[4] == mac_addr[4] && broadcastAddress[5] == mac_addr[5])
+  for(int i=0; i<data_len; i++)
   {
-    for(int i=0; i<data_len; i++)
-    {
-      Serial.write(data[i]);
-    }
-
-    channelHistory[2] = channelHistory[1];
-    channelHistory[1] = channelHistory[0];
-    channelHistory[0] = data[8];
+    Serial.write(data[i]);
   }
+
+  channelHistory[2] = channelHistory[1];
+  channelHistory[1] = channelHistory[0];
+  channelHistory[0] = data[8];
+    
+  flashLED = true;
 } 
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(460800);
 
   EEPROM.begin(512);
   EEPROM.get(0, startWebUpdater);
@@ -47,22 +48,30 @@ void setup()
 
     esp_now_register_recv_cb(OnDataRecv); 
   }
+
+  pinMode(WIFI_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  
+  flashLED = true;
 } 
 
 void loop()
-{
-  /*
-      Uncomment below to print mac address.  Add this to VRx setup().
-  */
-  // Serial.println(WiFi.macAddress());
-  // delay(1000);
-  
-  // A1, A2, A1 Select this channel order to start webupdater 
-  if (channelHistory[0] == 0 && channelHistory[1] == 1 && channelHistory[2] == 0)
+{  
+  // A1, A2, A1 Select this channel order to start webupdater
+  // Or press the boot button
+  if ( (channelHistory[0] == 0 && channelHistory[1] == 1 && channelHistory[2] == 0) || !digitalRead(WIFI_PIN) )
   {
     EEPROM.put(0, true);
     EEPROM.commit();  
-    Serial.println("Restarting in webupdater mode...");
+    
+    for (int i = 0; i < 5; i++)
+    {
+      digitalWrite(LED_PIN, LOW);
+      delay(50);
+      digitalWrite(LED_PIN, HIGH);
+      delay(50);
+    }
+
     delay(500);
     ESP.restart();
   }
@@ -70,5 +79,18 @@ void loop()
   if (startWebUpdater)
   {
     HandleWebUpdate();
+    flashLED = true;
+  }
+  
+  if (flashLED)
+  {
+    flashLED = false;
+    for (int i = 0; i < 4; i++)
+    {
+      digitalWrite(LED_PIN, LOW);
+      startWebUpdater == true ? delay(50) : delay(200);
+      digitalWrite(LED_PIN, HIGH);
+      startWebUpdater == true ? delay(50) : delay(200);
+    }
   }
 }
