01b16569e5019ac060fd133041850a49b13f5266
wvarty <wezley.varty@gmail.com>
2023-02-26 23:05:52 +1000

    Janky PoC for mavlink flow control. Mavlink lib should probably be a git submodule rather than the git repo itself

--- src/src/rx_main.cpp
+++ src/src/rx_main.cpp
@@ -25,6 +25,8 @@
 #include "devSerialUpdate.h"
 #include "devBaro.h"
 
+#include "common/mavlink.h"
+
 #if defined(PLATFORM_ESP8266)
 #include <FS.h>
 #elif defined(PLATFORM_ESP32)
@@ -1288,9 +1290,101 @@ static void HandleUARTout()
 {
     if (firmwareOptions.is_airport)
     {
+        // Software-based flow control for mavlink
+        uint16_t percentageFull = (apInputBuffer.size() * 100) / 600; // 2x mavlink packets at max size of ~300bytes each
+
+        if (OtaNonce % 64 == 1 || percentageFull > 50)
+        {
+            uint8_t rssi = 0;
+            uint8_t remrssi = 0;
+            uint8_t txbuf = 0;
+            uint8_t noise = 0;
+            uint8_t remnoise = 0;
+            uint16_t rxerrors = 0;
+            uint16_t fixed = 0;
+            
+            // Scale the actual buf percentage to a more agressive value
+            // Rates borrowed and adjusted from mLRS
+            if (percentageFull > 80)
+            {
+                txbuf = 0;      // ArduPilot:  0-19  -> +60 ms,    PX4:  0-24  -> *0.8
+            }
+            else if (percentageFull > 60)
+            {
+                txbuf = 30;     // ArduPilot: 20-49  -> +20 ms,    PX4: 25-34  -> *0.975
+            }
+            else if (percentageFull < 5)
+            {
+                txbuf = 100;    // ArduPilot: 96-100 -> -40 ms,    PX4: 51-100 -> *1.025
+            }
+            else if (percentageFull < 10)
+            {
+                txbuf = 91;     // ArduPilot: 91-95  -> -20 ms,    PX4: 51-100 -> *1.025
+            }
+            else
+            {
+                txbuf = 50;     // ArduPilot: 50-90  -> no change, PX4: 35-50  -> no change
+            }
+
+            uint8_t buf[MAVLINK_MSG_ID_RADIO_STATUS_LEN];
+            mavlink_message_t msg;
+            mavlink_msg_radio_status_pack(255, 0, &msg, rssi, remrssi, txbuf, noise, remnoise, rxerrors, fixed);
+            uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+            Serial.write(buf, len);
+
+            // RC channels code for later:
+
+            // uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+            // mavlink_message_t msg;
+            // mavlink_msg_rc_channels_override_pack(255, 0, &msg, 1, 1, CRSF_to_US(crsf.ChannelData[0]), CRSF_to_US(crsf.ChannelData[1]), CRSF_to_US(crsf.ChannelData[2]), CRSF_to_US(crsf.ChannelData[3]),1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,1000,1500,2000);
+            // uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+            // Serial.write(buf, len);
+        }
+
         while (apOutputBuffer.size())
         {
-            Serial.write(apOutputBuffer.pop());
+            uint8_t c = apOutputBuffer.pop();
+
+            mavlink_message_t msg;
+            mavlink_status_t status;
+
+            // Try parse a mavlink message
+            if (mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status))
+            {
+                // Message decoded successfully
+
+                // Check if its the ftp file request
+                if (msg.msgid == MAVLINK_MSG_ID_FILE_TRANSFER_PROTOCOL)
+                {
+                    // If it is, we want to deliberately hack the ftp url, so that the request fails
+                    // Borrowed and modified from mLRS
+                    uint8_t buf[MAVLINK_MSG_ID_FILE_TRANSFER_PROTOCOL_LEN];
+                    uint16_t len = mavlink_msg_file_transfer_protocol_get_payload(&msg, buf);
+
+                    uint8_t target_component = buf[0];
+                    uint8_t opcode = buf[3];
+                    char* url = (char*)(buf + 12);
+
+                    if (((target_component == MAV_COMP_ID_AUTOPILOT1) || (target_component == MAV_COMP_ID_ALL)) &&
+                        (opcode == MAV_FTP_OPCODE_OPENFILERO))
+                    {
+                        if (!strncmp(url, "@PARAM/param.pck", 16))
+                        {
+                            url[1] = url[7] = url[13] = 'x'; // now fake it to "@xARAM/xaram.xck"
+                            mavlink_message_t msg2;
+                            mavlink_msg_file_transfer_protocol_pack(msg.sysid, msg.compid, &msg2, 0, 0, target_component, (uint8_t*)url);
+                            uint16_t len = mavlink_msg_to_send_buffer(buf, &msg2);
+                            Serial.write(buf, len);
+                            return;
+                        }
+                    }
+                }
+                
+                // For all other mavlink messages, foward them on to the FC
+                uint8_t buf[MAVLINK_MAX_PAYLOAD_LEN];
+                uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+                Serial.write(buf, len);
+            }
         }
     }
 }
